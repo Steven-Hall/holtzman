7a3fd2966edbbeeb93df138ec769e1f19024a432
steven <steve@fancydash.io>
2019-11-28 23:29:06 +0800

    add setup.py and tox configuration

---
+++ setup.py
@@ -0,0 +1,15 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+setup(
+        name='holtzman',
+        license='MIT',
+        author='Steven Hall',
+        author_email='steve@fancydash.io',
+        version='0.0.1',
+        url='https://github.com/steven-hall/holtzman',
+        description='simple templating engine',
+        long_description=open('README.rst').read(),
+        packages=find_packages(exclude=['tests']),
+)
