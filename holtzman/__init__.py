e113d9fb34a0148711881ec027a7eaca92e7088c
steven <steve@fancydash.io>
2019-12-10 22:27:13 +0800

    merge Template and TemplateParser again

--- holtzman/__init__.py
+++ holtzman/__init__.py
@@ -3,21 +3,18 @@ from typing import TextIO
 
 from .template import Template
 from .template_source import TemplateSource
-from .template_parser import TemplateParser
 
 
 def from_string(source: str) -> "Template":
     source_stream: StringIO = StringIO(source)
     template_source = TemplateSource(source_stream)
-    template_parser = TemplateParser()
-    return template_parser.parse_template(template_source)
+    return Template(template_source)
 
 
 def from_file(source_file: str) -> "Template":
     source_stream: TextIO = open(source_file, 'r')
     try:
         template_source = TemplateSource(source_stream)
-        template_parser = TemplateParser()
-        return template_parser.parse_template(template_source)
+        return Template(template_source)
     finally:
         source_stream.close()
